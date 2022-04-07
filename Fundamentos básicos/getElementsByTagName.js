b88c73f4bdc2cc73a16c01e5a6e26ebb617c29c7
Robledogar <45006680+Robledogar@users.noreply.github.com>
2022-04-07 09:09:52 +0200

    Add files via upload

---
+++ Fundamentos básicos/getElementsByTagName.js
@@ -0,0 +1,16 @@
+//Acceso a los elementos con la misma etiqueta html (crea un array)
+
+window.onload = function() {
+	var div = document.getElementsByTagName("div");
+	//alert(div[1].innerHTML); //Esto accede al contenido html
+
+	div[1].style.backgroundColor = "red";//cambia el fondo del div indicado, a rojo
+	div[1].style.color = "blue";
+	div[1].style.padding = "20px";
+	div[1].style.margin = "20px";
+
+	var div3 = div[2];
+	div3.innerHTML = "<mark>Contenido cambiado</mark>";
+
+
+}
